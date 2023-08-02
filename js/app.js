4948b518e8f844db864d8ade6174c002a9ec251c
Mrinmoy Haloi <69071143+MrinmoyHaloi@users.noreply.github.com>
2023-08-02 23:12:17 +0530

    feat: redesign landing page

--- js/app.js
+++ js/app.js
@@ -19,19 +19,6 @@ function splitText(element, item_class) {
 
 splitText(".split-text", "split-item");
 
-const headingDiv = document.querySelector(".heading");
-let spans = document.querySelectorAll(".heading span");
-
-spans.forEach((span) => {
-    // add animated class when span is hovered
-    span.addEventListener("mouseover", () => {
-        span.classList.add("animated");
-    });
-    span.addEventListener("animationend", () => {
-        span.classList.remove("animated");
-    });
-});
-
 // .project-[link,title,desc,log,stars,langs]
 // get all the projects and then get the data-project attribute
 const projects = document.querySelectorAll(".project");
@@ -90,150 +77,6 @@ let afterShadow =
     "1px 1px 0px #4a00e0, 2px 2px 0px #4a00e0, 3px 3px 0px #4a00e0, 4px 4px 0px #4a00e0, 5px 5px 0px #4a00e0, 6px 6px 0px #4a00e0, 7px 7px 0px #4a00e0, 8px 8px 0px #4a00e0, 9px 9px 0px #4a00e0, 10px 10px 0px #4a00e0";
 
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-
-// tl.to(".text", { opacity: "100%", duration: 0.7 });
-// tl.to(".text", { x: "0%", duration: 1.3 }, "-=.5");
-// tl.to(".text", { y: "0%", duration: 1.3, stagger: 0.25 }, "-=2");
-// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 3.5 });
-// tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-
-tl.fromTo(
-    ".first-text span",
-    {
-        textShadow: beforeShadow,
-    },
-    {
-        textShadow: afterShadow,
-        duration: 0.31,
-        stagger: 0.1,
-    },
-    "-=.6"
-);
-tl.fromTo(
-    ".second-text span",
-    {
-        textShadow: beforeShadow,
-    },
-    {
-        textShadow: afterShadow,
-        duration: 0.3,
-        stagger: 0.1,
-    },
-    "-=.7"
-);
-
-tl.fromTo(
-    ".third-text span",
-    {
-        textShadow: beforeShadow,
-    },
-    {
-        textShadow: afterShadow,
-        duration: 0.3,
-        stagger: 0.1,
-    },
-    "-=1.3"
-);
-
-gsap.to(".third-text span", {
-    scrollTrigger: {
-        trigger: ".third-text",
-        start: "top center",
-        end: "top center",
-        toggleActions: "play none reverse none",
-    },
-    rotation: 360,
-    duration: 1,
-    stagger: 0.1,
-});
-
-gsap.fromTo(
-    ".about-title span",
-    {
-        translateY: -20,
-    },
-    {
-        scrollTrigger: {
-            trigger: ".about-title",
-            start: "top-=100 center",
-            end: "top-=100 center",
-            toggleActions: "play none reverse none",
-        },
-        translateY: 0,
-        duration: 0.3,
-        stagger: 0.1,
-    }
-);
-
-gsap.fromTo(
-    ".skill-title span",
-    {
-        translateY: -20,
-    },
-    {
-        scrollTrigger: {
-            trigger: ".skill-title",
-            start: "top-=100 center",
-            end: "top-=100 center",
-            toggleActions: "play none reverse none",
-        },
-        translateY: 0,
-        duration: 0.3,
-        stagger: 0.1,
-    }
-);
-
-gsap.fromTo(
-    ".project-title span",
-    {
-        translateY: -20,
-    },
-    {
-        scrollTrigger: {
-            trigger: ".project-title",
-            start: "top-=100 center",
-            end: "top-=100 center",
-            toggleActions: "play none reverse none",
-        },
-        translateY: 0,
-        duration: 0.3,
-        stagger: 0.1,
-    }
-);
-gsap.fromTo(
-    ".message-title span",
-    {
-        translateY: -20,
-    },
-    {
-        scrollTrigger: {
-            trigger: ".message-title",
-            start: "top-=100 center",
-            end: "top-=100 center",
-            toggleActions: "play none reverse none",
-        },
-        translateY: 0,
-        duration: 0.3,
-        stagger: 0.1,
-    }
-);
-gsap.fromTo(
-    ".get-in-touch-title span",
-    {
-        translateY: -20,
-    },
-    {
-        scrollTrigger: {
-            trigger: ".get-in-touch-title",
-            start: "top-=100 center",
-            end: "top-=100 center",
-            toggleActions: "play none reverse none",
-        },
-        translateY: 0,
-        duration: 0.3,
-        stagger: 0.1,
-    }
-);
 
 gsap.fromTo(
     ".language, .tool",
@@ -309,8 +152,6 @@ const texts = [
     "LINUX",
 ];
 const options = { radius: 230, maxSpeed: "fast" };
-
-// TagCloud(container, texts, options);
 
 var TagOptions = {
     textColour: "#fff",
