fd5752ee0f84937f267764007d4a11937360c310
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2024-12-10 22:46:35 +0530

    fix: improve footer styling and hover effects

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,19 @@
 
 <style lang="scss">
 footer {
+	display: grid;
+	place-items: center;
+	padding: 1rem;
 	p {
+		width: fit-content;
 		font-size: 1rem;
 		font-weight: 500;
+		color: hsl(180, 78%, 43%);
+		padding: 0;
+		cursor: default;
+		&:hover {
+			color: hsl(180, 78%, 63%);
+		}
 	}
 }
 </style>
