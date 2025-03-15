fbf68aebe87058606f21f6a23bbe13b87edb3de1
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2025-03-16 02:52:37 +0530

    fix: remove footer font color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,12 +18,8 @@ footer {
 		width: fit-content;
 		font-size: 1rem;
 		font-weight: 500;
-		color: hsl(180, 78%, 43%);
 		padding: 0;
 		cursor: default;
-		&:hover {
-			color: hsl(180, 78%, 63%);
-		}
 	}
 }
 </style>
