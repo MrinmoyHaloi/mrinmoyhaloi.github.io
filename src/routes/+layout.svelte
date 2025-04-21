07b9f821337ae5d6bb4193e7b1dff6b9de9cc4dc
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2025-04-21 21:21:34 +0530

    fix: add outline to footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 {@render children()}
 
 <footer>
-	<p class="text-center p-4">Mrinmoy Haloi 2025</p>
+	<p class="p-4 text-center">Mrinmoy Haloi 2025</p>
 </footer>
 
 <style lang="scss">
@@ -14,6 +14,7 @@ footer {
 	display: grid;
 	place-items: center;
 	padding: 1rem;
+	outline: 1px solid #155e75;
 	p {
 		width: fit-content;
 		font-size: 1rem;
