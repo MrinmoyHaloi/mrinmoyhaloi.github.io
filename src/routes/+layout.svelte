475b6d80d1eb8877c9beccc6ec273ede42eaebb8
MrinmoyHaloi <69071143+MrinmoyHaloi@users.noreply.github.com>
2026-06-07 19:53:00 +0530

    fix: back to top button visibility on phone

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,9 +7,10 @@
 {@render children()}
 
 <footer>
-	<p class="p-4 text-center">Crafted by <b class="text-white">Mrinmoy Haloi</b> 2026</p>
-	<a href="#home" class="text-white hover:text-blue-500"
-		>Back to top <iconify-icon icon="fa6-solid:arrow-up"></iconify-icon> </a>
+	<p class="p-4 text-center"><span>Crafted by</span> <span><b class="text-white">Mrinmoy Haloi</b> 2026</span></p>
+	<a href="#home" class="text-white hover:text-blue-500" aria-label="Back to top link"
+		><iconify-icon icon="fa6-solid:arrow-up"></iconify-icon>
+	</a>
 </footer>
 
 <style lang="scss">
@@ -23,10 +24,13 @@
 		a {
 			color: #d5d5d5;
 			position: absolute;
-			right: 1%;
+			right: 3%;
 			display: flex;
 			align-items: center;
 			gap: 0.5rem;
+			@media (width < 500px) {
+				right: 5%;
+			}
 			&:hover {
 				color: hsl(180, 100%, 35%);
 			}
@@ -36,6 +40,10 @@
 			font-weight: 500;
 			padding: 0;
 			color: #d5d5d5;
+			@media (width < 500px) {
+				display: grid;
+				line-height: 2rem;
+			}
 		}
 	}
 </style>
